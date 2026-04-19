9e0c80f932f7d082f9bf8eec196b61b6de3306ff
meny2333 <meny233@qq.com>
2026-04-19 08:57:51 +0800

    可以

--- addons/ugc_import/plugin.gd
+++ addons/ugc_import/plugin.gd
@@ -142,11 +142,85 @@ func _import_level(meta: Dictionary, do_pack_pck: bool) -> void:
 	
 	DirAccess.make_dir_recursive_absolute(target_dir)
 	_copy_directory_recursive(source_dir, target_dir)
+	_import_missing_template_resources(source_dir)
 	
 	if do_pack_pck:
 		_pack_to_pck(level_name, target_dir)
 	
 	print("Imported: ", level_name, " -> ", target_dir)
+
+func _import_missing_template_resources(source_dir: String) -> void:
+	var dir := DirAccess.open(source_dir)
+	if dir == null:
+		return
+	dir.list_dir_begin()
+	var name := dir.get_next()
+	while name != "":
+		if dir.current_is_dir() and not name.begins_with("."):
+			_import_missing_template_resources(source_dir.path_join(name))
+		elif name.ends_with(".tscn") or name.ends_with(".tres"):
+			_copy_missing_resources(source_dir.path_join(name), source_dir)
+		name = dir.get_next()
+	dir.list_dir_end()
+
+func _copy_missing_resources(file_path: String, source_dir: String) -> void:
+	var file := FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		return
+	var content := file.get_as_text()
+	file.close()
+	
+	var regex := RegEx.new()
+	regex.compile('\\[ext_resource[^\\]]*path="([^"]+)"[^\\]]*\\]')
+	var matches := regex.search_all(content)
+	for m in matches:
+		var res_path := m.get_string(1)
+		if res_path.begins_with("res://") and not res_path.begins_with("res://levels/"):
+			var global_path := ProjectSettings.globalize_path(res_path)
+			if not FileAccess.file_exists(global_path):
+				_try_copy_from_source(res_path, source_dir)
+
+func _try_copy_from_source(res_path: String, source_dir: String) -> void:
+	var relative_path := res_path.replace("res://", "")
+	var template_pos := source_dir.find("#Template")
+	var source_project_root: String
+	if template_pos >= 0:
+		source_project_root = source_dir.left(template_pos).path_join("")
+	else:
+		source_project_root = source_dir.get_base_dir().get_base_dir()
+	var source_file := source_project_root.path_join(relative_path)
+	var dest_file := ProjectSettings.globalize_path(res_path)
+	
+	if not FileAccess.file_exists(source_file):
+		push_warning("Source not found: " + source_file)
+		return
+	
+	DirAccess.make_dir_recursive_absolute(dest_file.get_base_dir())
+	DirAccess.copy_absolute(source_file, dest_file)
+	print("  Copied: ", res_path)
+	
+	var import_src := source_file + ".import"
+	var import_dst := dest_file + ".import"
+	if FileAccess.file_exists(import_src):
+		DirAccess.copy_absolute(import_src, import_dst)
+		print("  Copied import: ", res_path + ".import")
+		_copy_imported_cache_files(import_src, source_project_root)
+
+func _copy_imported_cache_files(import_file_path: String, source_project_root: String) -> void:
+	var import_file := ConfigFile.new()
+	if import_file.load(import_file_path) != OK:
+		return
+	
+	if import_file.has_section_key("remap", "path.s3tc"):
+		var cache_path: String = str(import_file.get_value("remap", "path.s3tc", ""))
+		if cache_path != "":
+			var cache_relative: String = cache_path.replace("res://", "")
+			var cache_src: String = source_project_root.path_join(cache_relative)
+			var cache_dst: String = ProjectSettings.globalize_path(cache_path)
+			if FileAccess.file_exists(cache_src):
+				DirAccess.make_dir_recursive_absolute(cache_dst.get_base_dir())
+				DirAccess.copy_absolute(cache_src, cache_dst)
+				print("  Copied cache: ", cache_path)
 
 func _copy_directory_recursive(source: String, target: String) -> void:
 	var dir := DirAccess.open(source)
@@ -173,8 +247,70 @@ func _pack_to_pck(level_name: String, level_dir: String) -> void:
 	if packer.pck_start(pck_path) != OK:
 		return
 	_add_dir_to_pck(packer, level_dir, "levels/" + level_name)
+	_add_template_resources(packer, level_dir)
 	packer.flush()
 	print("Packed: ", pck_path)
+
+func _add_template_resources(packer: PCKPacker, level_dir: String) -> void:
+	var dir := DirAccess.open(level_dir)
+	if dir == null:
+		return
+	dir.list_dir_begin()
+	var name := dir.get_next()
+	while name != "":
+		if dir.current_is_dir() and not name.begins_with("."):
+			_add_template_resources(packer, level_dir.path_join(name))
+		elif name.ends_with(".tscn") or name.ends_with(".tres"):
+			var file_path := level_dir.path_join(name)
+			_pack_external_resources(packer, file_path)
+		name = dir.get_next()
+	dir.list_dir_end()
+
+func _pack_external_resources(packer: PCKPacker, file_path: String) -> void:
+	var file := FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		return
+	var content := file.get_as_text()
+	file.close()
+	
+	var regex := RegEx.new()
+	regex.compile('\\[ext_resource[^\\]]*path="([^"]+)"[^\\]]*\\]')
+	var matches := regex.search_all(content)
+	for m in matches:
+		var res_path := m.get_string(1)
+		if res_path.begins_with("res://") and not res_path.begins_with("res://levels/"):
+			var global_path := ProjectSettings.globalize_path(res_path)
+			if FileAccess.file_exists(global_path):
+				packer.add_file(res_path, global_path)
+				print("  Added template resource: ", res_path)
+				_pack_imported_cache(packer, res_path)
+			else:
+				push_warning("  Template resource not found: " + res_path)
+
+func _pack_imported_cache(packer: PCKPacker, res_path: String) -> void:
+	var import_path := res_path + ".import"
+	var import_global := ProjectSettings.globalize_path(import_path)
+	if not FileAccess.file_exists(import_global):
+		return
+	
+	var import_file := ConfigFile.new()
+	if import_file.load(import_global) != OK:
+		return
+	
+	if import_file.has_section_key("remap", "path.s3tc"):
+		var cache_path := import_file.get_value("remap", "path.s3tc", "")
+		if cache_path != "":
+			var cache_global := ProjectSettings.globalize_path(cache_path)
+			if FileAccess.file_exists(cache_global):
+				packer.add_file(cache_path, cache_global)
+				print("  Added imported cache: ", cache_path)
+	
+	if import_file.has_section_key("deps", "dest_files"):
+		var dest_files: PackedStringArray = import_file.get_value("deps", "dest_files", [])
+		for dest in dest_files:
+			var dest_global := ProjectSettings.globalize_path(dest)
+			if FileAccess.file_exists(dest_global):
+				packer.add_file(dest, dest_global)
 
 func _add_dir_to_pck(packer: PCKPacker, dir_path: String, prefix: String) -> void:
 	var dir := DirAccess.open(dir_path)
